80dc0c5f29782f33f8020c202460fd72dac0df5e
ubk8751 <ubk8751@gmail.com>
2023-11-30 14:36:16 +0100

    all but printing tasks works

--- app/src/api/api.py
+++ app/src/api/api.py
@@ -1,12 +1,44 @@
-from flask import Flask, jsonify, request, render_template, redirect, flash
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
-import time
+"""
+Flask Application
+
+This module defines a Flask web application for managing tasks. It includes routes
+for rendering HTML templates, handling API requests, and interacting with a database
+to perform CRUD operations on tasks.
+
+The application uses Flask for web development, SQLAlchemy for database interaction,
+and models from the 'models' module to represent the data structure.
+
+Note:
+    Ensure that the required environment variables such as DB_HOST, DB_PORT, and DB_NAME
+    are properly configured before running this application.
+
+Example:
+    To run the application:
+        $ python app.py
+
+Attributes:
+    app (Flask): The Flask application instance.
+    DB_HOST (str): The database host obtained from environment variables.
+    DB_PORT (str): The database port obtained from environment variables.
+    DB_NAME (str): The database name obtained from environment variables.
+
+Routes:
+    - '/' : Renders the index template.
+    - '/tasks' : Handles tasks-related API requests.
+        - GET: Retrieve all tasks.
+        - POST: Add a new task.
+    - '/tasks/add' : Handles the addition of a new task.
+        - POST: Add a new task to the database.
+    - '/tasks/delete/<int:task_id>' : Handles the deletion of a task.
+        - GET: Delete a task based on its ID.
+
+"""
 import os
+from flask import Flask, jsonify, request, render_template
+from models import db, Task
 
-from models import db, User, Task
-
-app = Flask(__name__, template_folder='../ui/templates', static_folder='../ui/static')
+app = Flask(__name__, template_folder='../ui/templates',
+            static_folder='../ui/static')
 
 DB_HOST = os.environ.get('DB_HOST', 'postgresql_db')
 DB_PORT = os.environ.get('DB_PORT', '5432')
@@ -18,44 +50,74 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 db.init_app(app)
 
 with app.app_context():
-    #db.session.execute(text('DROP TABLE IF EXISTS "user", "task" CASCADE'))
+    # db.session.execute(text('DROP TABLE IF EXISTS "user", "task" CASCADE'))
     db.create_all()
+
 
 @app.route('/')
 def index():
+    """
+    Render the index template.
+
+    Returns:
+        str: Rendered HTML for the index page.
+    """
     return render_template('index.html')
+
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
+    """
+    Retrieve all tasks.
+
+    Returns:
+        tuple: A tuple containing a JSON response with tasks and a status code.
+    """
     tasks = Task.query.all()
-    return render_template('tasks.html', tasks=tasks)
+    return jsonify({'tasks': [{
+        'task': task.task,
+        'description': task.description
+    } for task in tasks]}), 200
+
 
 @app.route('/tasks/add', methods=['POST'])
 def add_task():
-    data = request.form    
-    user = User.query.first()
-    print(user)
-    if user == None:
-        new_user = User(username='John Doe', email='john.doe@test.com')
-        db.session.add(new_user)
-        db.session.commit()
-        flash('No users are registered. Adding default user for testing purposes.')
-        return redirect('/tasks')
-    elif user:
-        new_task = Task(task=data['task'], userid=user.id, description=data['description'])
-        db.session.add(new_task)
-        db.session.commit()
-        return redirect('/tasks')
-    else:
-        return redirect('/tasks')
+    """
+    Add a new task.
+
+    Returns:
+        tuple: A tuple containing a JSON response with a success message and a status code.
+    """
+    data = request.get_json()
+    print(data)
+    new_task = Task(
+        task=data['content']['task'],
+        userid=data['user'],
+        description=data['content']['description']
+    )
+    db.session.add(new_task)
+    db.session.commit()
+
+    tasks = Task.query.all()
+    print(tasks)
+    return jsonify({'message': 'Task added successfully', 'tasks': [{'id': task.id, 'task': task.task, 'description': task.description} for task in tasks]}), 200
 
 @app.route('/tasks/delete/<int:task_id>', methods=['GET', 'POST'])
 def delete_task(task_id):
+    """
+    Delete a task.
+
+    Args:
+        task_id (int): The ID of the task to be deleted.
+
+    Returns:
+        tuple: A tuple containing a JSON response with a success message and a status code.
+    """
     task = Task.query.get_or_404(task_id)
     db.session.delete(task)
     db.session.commit()
-    flash('Task deleted successfully.')
-    return redirect('/tasks')
+    return jsonify({'message': 'Task deleted successfully.'}), 200
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5002)
--- app/src/api/models.py
+++ app/src/api/models.py
@@ -2,15 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id          = db.Column(db.Integer, primary_key=True)
-    username    = db.Column(db.String(80), unique=True, nullable=False)
-    email       = db.Column(db.String(120), unique=True, nullable=False)
-    password    = db.Column(db.String(80), unique=True, nullable=False)
-    tasks       = db.relationship('Task', backref='user', lazy=True)
 
 class Task(db.Model):
-    id          = db.Column(db.Integer, primary_key=True)
-    userid      = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    task        = db.Column(db.String(80), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, nullable=False)
+    task = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(360), nullable=False)
